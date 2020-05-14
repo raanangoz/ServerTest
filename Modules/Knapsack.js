bc6be49e0f9276ae1e494465aca8f05a58e43910
osherhe <44405697+osherhe@users.noreply.github.com>
2020-05-14 20:11:51 +0300

    getInstanceCounters ad

--- Modules/Knapsack.js
+++ Modules/Knapsack.js
@@ -311,10 +311,10 @@ Knapsack.get('/getInstancesCounters5', function (req, res) {
 Knapsack.post('/updateInstancePresentation',function (req, res) {
     var currentCounter = req.body.counter - 1;
     var puzzleID = req.body.puzzleID;
-    var query = "update KSinstance set  "+regularPresCounter+"= '"+currentCounter+"' where PuzzleID= '"+PuzzleID+"'";
-    //var postQuery = "update KSinstance set  regularPresCounter= '"+currentCounter+"' where PuzzleID= '"+PuzzleID+"'";
+    //var query = "update KSinstance set  "+regularPresCounter+"= '"+currentCounter+"' where PuzzleID= '"+PuzzleID+"'";
+    var postQuery = "update KSinstance set  regularPresCounter= '"+currentCounter+"' where PuzzleID= '"+PuzzleID+"'";
 
-    DButilsAzure.execQuery(query)
+    DButilsAzure.execQuery(postQuery)
 
     // // var query = "select orderPOI from userData where userName='"+username+"'";
         .then(function (result) {
